f234edcad8c870ae126c11ede4e7699c67ac48a0
Ansh <jndl.ansh@gmail.com>
2025-11-25 14:57:52 +0530

    Implement daily news summary

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -1,4 +1,3 @@
-import { log } from 'console';
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -24,3 +24,21 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
 
     await transporter.sendMail(mailOptions);
 }
+
+export const sendNewsSummaryEmail = async (
+    { email, date, newsContent }: { email: string; date: string; newsContent: string }
+): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', date)
+        .replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: `"Stocker" <sheshh.serpent@gmail.com>`,
+        to: email,
+        subject: `📈 Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Stocker.`,
+        html: htmlTemplate,
+    };
+
+    await transporter.sendMail(mailOptions);
+}; 
